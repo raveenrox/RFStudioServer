9ea2b36250479d10c44ec8701033c9769305e4dc
raveenrox <raveenrox@gmail.com>
2015-10-12 10:03:48 +0530

    build 1.0 bug fix switch case fix

---
+++ RFStudioServer/mainUI.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace RFStudioServer
+{
+    partial class mainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainUI));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.gbSocket = new System.Windows.Forms.GroupBox();
+            this.gbAccount = new System.Windows.Forms.GroupBox();
+            this.btnSaveAccount = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnSaveSettings = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cbAuto = new System.Windows.Forms.CheckBox();
+            this.cbMin = new System.Windows.Forms.CheckBox();
+            this.gbSocket.SuspendLayout();
+            this.gbAccount.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 45);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(14, 22);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(71, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "27015";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(96, 45);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // gbSocket
+            // 
+            this.gbSocket.Controls.Add(this.txtPort);
+            this.gbSocket.Controls.Add(this.btnStop);
+            this.gbSocket.Controls.Add(this.btnStart);
+            this.gbSocket.Controls.Add(this.lblPort);
+            this.gbSocket.Location = new System.Drawing.Point(12, 12);
+            this.gbSocket.Name = "gbSocket";
+            this.gbSocket.Size = new System.Drawing.Size(181, 77);
+            this.gbSocket.TabIndex = 4;
+            this.gbSocket.TabStop = false;
+            this.gbSocket.Text = "Socket";
+            // 
+            // gbAccount
+            // 
+            this.gbAccount.Controls.Add(this.btnSaveAccount);
+            this.gbAccount.Controls.Add(this.txtPassword);
+            this.gbAccount.Controls.Add(this.lblPassword);
+            this.gbAccount.Controls.Add(this.txtUsername);
+            this.gbAccount.Controls.Add(this.lblUsername);
+            this.gbAccount.Location = new System.Drawing.Point(12, 95);
+            this.gbAccount.Name = "gbAccount";
+            this.gbAccount.Size = new System.Drawing.Size(181, 104);
+            this.gbAccount.TabIndex = 5;
+            this.gbAccount.TabStop = false;
+            this.gbAccount.Text = "Account";
+            // 
+            // btnSaveAccount
+            // 
+            this.btnSaveAccount.Location = new System.Drawing.Point(60, 71);
+            this.btnSaveAccount.Name = "btnSaveAccount";
+            this.btnSaveAccount.Size = new System.Drawing.Size(111, 23);
+            this.btnSaveAccount.TabIndex = 8;
+            this.btnSaveAccount.Text = "Save Account";
+            this.btnSaveAccount.UseVisualStyleBackColor = true;
+            this.btnSaveAccount.Click += new System.EventHandler(this.btnSaveAccount_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(71, 45);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.Text = "1234";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(14, 48);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(71, 19);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.Text = "raveen";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(14, 22);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 4;
+            this.lblUsername.Text = "Username";
+            // 
+            // btnSaveSettings
+            // 
+            this.btnSaveSettings.Location = new System.Drawing.Point(82, 232);
+            this.btnSaveSettings.Name = "btnSaveSettings";
+            this.btnSaveSettings.Size = new System.Drawing.Size(111, 23);
+            this.btnSaveSettings.TabIndex = 4;
+            this.btnSaveSettings.Text = "Save Settings";
+            this.btnSaveSettings.UseVisualStyleBackColor = true;
+            this.btnSaveSettings.Click += new System.EventHandler(this.btnSaveSettings_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Location = new System.Drawing.Point(12, 258);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(181, 20);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "This application is now minimized";
+            this.notifyIcon.BalloonTipTitle = "RFStudio Server Application";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "RFStudio Server Application";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.BalloonTipClicked += new System.EventHandler(this.notifyIcon_BalloonTipClicked);
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // cbAuto
+            // 
+            this.cbAuto.AutoSize = true;
+            this.cbAuto.Location = new System.Drawing.Point(13, 209);
+            this.cbAuto.Name = "cbAuto";
+            this.cbAuto.Size = new System.Drawing.Size(68, 17);
+            this.cbAuto.TabIndex = 10;
+            this.cbAuto.Text = "Autostart";
+            this.cbAuto.UseVisualStyleBackColor = true;
+            // 
+            // cbMin
+            // 
+            this.cbMin.AutoSize = true;
+            this.cbMin.Location = new System.Drawing.Point(87, 209);
+            this.cbMin.Name = "cbMin";
+            this.cbMin.Size = new System.Drawing.Size(97, 17);
+            this.cbMin.TabIndex = 11;
+            this.cbMin.Text = "Start Minimized";
+            this.cbMin.UseVisualStyleBackColor = true;
+            // 
+            // mainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(205, 284);
+            this.Controls.Add(this.cbMin);
+            this.Controls.Add(this.cbAuto);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSaveSettings);
+            this.Controls.Add(this.gbAccount);
+            this.Controls.Add(this.gbSocket);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RFStudioServer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainUI_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainUI_FormClosed);
+            this.Resize += new System.EventHandler(this.mainUI_Resize);
+            this.gbSocket.ResumeLayout(false);
+            this.gbSocket.PerformLayout();
+            this.gbAccount.ResumeLayout(false);
+            this.gbAccount.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox gbSocket;
+        private System.Windows.Forms.GroupBox gbAccount;
+        private System.Windows.Forms.Button btnSaveAccount;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnSaveSettings;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.CheckBox cbAuto;
+        private System.Windows.Forms.CheckBox cbMin;
+    }
+}
+
---
+++ RFStudioServer/mainUI.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RFStudioServer
+{
+    public partial class mainUI : Form
+    {
+        private PrivateSocket socket;
+        private Thread socketThread;
+        private String username = "";
+        private String password = "";
+
+        public mainUI()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+            prefLoad();
+            loadAccount();
+            if(cbAuto.Checked)
+            {
+                socketStart();
+            }
+            if (cbMin.Checked)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            socketStart();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //TODO FixMe
+            socketStop();
+            notifyIcon.Visible = false;
+            Environment.Exit(0);
+        }
+        private void btnSaveAccount_Click(object sender, EventArgs e)
+        {
+            loadAccount();
+            lblStatus.Text = "Account Details Saved";
+
+        }
+
+        private void btnSaveSettings_Click(object sender, EventArgs e)
+        {
+            savePref();
+        }
+
+        private void mainUI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Environment.Exit(0);
+        }
+
+        private void mainUI_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                hideForm();
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            showForm();
+        }
+
+        private void mainUI_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notifyIcon.Visible = false;
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                showForm();
+            }
+            else
+            {
+                hideForm();
+            }
+        }
+
+        private void notifyIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            showForm();
+        }
+
+        public String processData(String data)
+        {
+            //TODO UPDATE THIS
+            /**************************/
+            /***EVERYTHING GOES HERE***/
+            /**USE 'data' TO GET TEXT**/
+            /**vv**vv**vv**vv**vv**vv**/
+            if(data.StartsWith("ping"))
+            {
+                return "SUCCESSFUL";
+            }
+            else
+            {
+                return "EMPTY_RESPONSE : " + data;
+            }
+        }
+
+        private void socketStart()
+        {
+            socket = new PrivateSocket(this, Int32.Parse(txtPort.Text));
+            socketThread = new Thread(socket.StartListening);
+            socketThread.Start();
+            btnStart.Enabled = false;
+            txtPort.Enabled = false;
+            btnStop.Enabled = true;
+            gbAccount.Enabled = false;
+            lblStatus.Text = "Socket Started";
+        }
+
+        private void socketStop()
+        {
+            socket.stopConnection();
+            socketThread.Abort();
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+            txtPort.Enabled = true;
+            gbAccount.Enabled = true;
+            lblStatus.Text = "Socket Stopped";
+        }
+
+
+        public bool accountVerify(string data)
+        {
+            String usr = "";
+            String pwd = "";
+            char[] arr;
+            arr = data.ToCharArray();
+            usr = data.Substring(0, data.IndexOf(":"));
+            pwd = data.Substring(data.IndexOf(":") + 1);
+
+            if (usr == this.username)
+            {
+                if (pwd == this.password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void logError(Exception e)
+        {
+            File.AppendAllText("ErrorLog.log", "\n" + DateTime.Now + "\t" + e.ToString() + e.StackTrace);
+        }
+
+        public void logError(string line)
+        {
+            File.AppendAllText("ErrorLog.log", "\n" + DateTime.Now + "\t" + line);
+        }
+
+        public void logData(string line)
+        {
+            File.AppendAllText("IncomingLog.log", "\n" + DateTime.Now + "\t" + line);
+        }
+
+        private void loadAccount()
+        {
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+        }
+
+        private void prefLoad()
+        {
+            string xml = "";
+            try
+            {
+                xml = Base64Decode(File.ReadAllText("pref.bin"));
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xml);
+
+                XmlNode usernameNode = xmlDoc.GetElementsByTagName("username")[0];
+                XmlNode passwordNode = xmlDoc.GetElementsByTagName("password")[0];
+                XmlNode portNode = xmlDoc.GetElementsByTagName("port")[0];
+                XmlNode autoStartNode = xmlDoc.GetElementsByTagName("auto")[0];
+                XmlNode minimizedNode = xmlDoc.GetElementsByTagName("minimized")[0];
+
+                txtUsername.Text = usernameNode.InnerText;
+                txtPassword.Text = Base64Decode(passwordNode.InnerText);
+                txtPort.Text = portNode.InnerText;
+                if(autoStartNode.InnerText=="True")
+                {
+                    cbAuto.Checked = true;
+                }else if(autoStartNode.InnerText=="False")
+                {
+                    cbAuto.Checked = false;
+                }
+                if(minimizedNode.InnerText=="True")
+                {
+                    cbMin.Checked = true;
+                }else if (minimizedNode.InnerText == "False")
+                {
+                    cbMin.Checked = false;
+                }
+
+            }
+            catch (Exception e) { logError(e); }
+        }
+
+        private void savePref()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlElement element = (XmlElement)xmlDoc.AppendChild(xmlDoc.CreateElement("details"));
+            element.AppendChild(xmlDoc.CreateElement("username")).InnerText = txtUsername.Text;
+            element.AppendChild(xmlDoc.CreateElement("password")).InnerText = Base64Encode(txtPassword.Text);
+            element.AppendChild(xmlDoc.CreateElement("port")).InnerText = txtPort.Text;
+            element.AppendChild(xmlDoc.CreateElement("auto")).InnerText = cbAuto.Checked.ToString();
+            element.AppendChild(xmlDoc.CreateElement("minimized")).InnerText = cbMin.Checked.ToString();
+            File.WriteAllText("pref.bin", Base64Encode(xmlDoc.OuterXml));
+            lblStatus.Text = "Preferences Saved";
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public void updateStatus(String line)
+        {
+            if (lblStatus.InvokeRequired)
+            {
+                lblStatus.Invoke(new Action(() => lblStatus.Text = line));
+            }
+        }
+
+        private void showForm()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void hideForm()
+        {
+            if (socket != null)
+            {
+                notifyIcon.BalloonTipText = "The server is running in background";
+            }
+            else
+            {
+                notifyIcon.BalloonTipText = "The server is stopped";
+            }
+            //notifyIcon.ShowBalloonTip(500);
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+    }
+}
